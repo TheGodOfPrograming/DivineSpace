8466866a3d4195a7077a8dfe49961ef0d6f11813
moise <moise@Godcomputer.mshome.net>
2026-02-04 09:36:11 +0100

    Hola2

--- Proyecto1/src/paquete1/principal.java
+++ Proyecto1/src/paquete1/principal.java
@@ -6,7 +6,7 @@ public class principal {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo feo y cruel");
 		System.out.println("Pensaste que acabarías conmigo, ¿verdad?");
-		System.out.println("Pero aqui estoy más fuerte que nunca. \n      ¡¡¡¡¡Preparate que se viene un paliza!!!!!!");
+		System.out.println("Pero aqui estoy más fuerte que nunca. \n      ¡¡¡¡¡Preparate que se viene una paliza!!!!!!");
 	}
 
 }
